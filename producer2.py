4682223821ea56c13efd6db048bd43ab01a57f47
Pallavi7Vijay <56357885+Pallavi7Vijay@users.noreply.github.com>
2021-12-20 11:37:07 +0530

    Add files via upload

---
+++ producer2.py
@@ -0,0 +1,45 @@
+import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from kafka import KafkaProducer
+
+#Enter keys here
+consumer_key    = ''
+consumer_secret = ''
+access_token    = ''
+access_token_secret   = ''
+
+
+producer = KafkaProducer(
+bootstrap_servers='localhost:9092'
+,value_serializer=lambda x: x.encode('utf-8')
+) #Same port as your Kafka server
+
+
+topic_name = "Twitter"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+class Producer(StreamListener):
+
+    def __init__(self, producer):
+        self.producer = producer
+
+    def on_data(self, data):
+        self.producer.send(topic_name, value=data,partition=1)
+        return True
+
+    def on_error(self, error):
+        print(error)
+
+twitter_stream = Stream(auth, Producer(producer))
+twitter_stream.filter(track=["Trump"])  #Stream Trump tweets
+
+
+
+
+                                                                                                                                                                                   
